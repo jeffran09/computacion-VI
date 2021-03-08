5e59c70014976fc2abaedd09d8cebec89754755b
francisco gomez <gomezrfco@gmail.com>
2021-03-08 17:59:24 -0400

    metodo pos con 50 json personales

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
+// ejercio de json numero 4 I corte. 
+//el ejemplo.json el array fue proporcionado por /albums en la pagina de https://jsonplaceholder.typicode.com/
+
+
+
+const fs = require('fs'); //me permite leer, crear, actualizar, borrar archivos
+let rawData = fs.readFileSync('ejemplo.json');
+let json = JSON.parse(rawData);
+
+
 const axios = require('axios'); //accion para ejecutar get y post  
 
-
-axios.get('https://api.covid19api.com/summary')
-.then(pregunta => {   
-    console.log ("el get es", pregunta.data.Global); //imprimo mi dato global donde se encuentra todo los relacionado del covib19. METODO GET
+axios.post('https://jsonplaceholder.typicode.com/posts', json,  {
     
-})    
-
-axios.post('https://jsonplaceholder.typicode.com/comments?postId=1', {
-    pais: "Canada",
-    contraseña: "123312",
 }) //datos que envio a la API para que me genere una respuesta en el panel serial. METODO POST
 .then(pedido => {
-    console.log("la peticion post es ", pedido.data);
-})
+    console.log("la primera peticion es:", pedido.data[0]);
+    console.log("la ultima peticion es: " , pedido.data[49]);
+}) //en este post se muestra un espejo en su respuesta, donde se pide informacion del primero al ultimo
+.catch(error => {
+    console.log(error);
+} )
